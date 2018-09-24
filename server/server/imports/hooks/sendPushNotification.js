b8bbd0637f4aeaf2eee77dfddb71b41f579b129e
Joaquín Gumucio <jjgumucio@gmail.com>
2018-09-24 17:46:18 -0300

    testing push notifications

--- server/server/imports/hooks/sendPushNotification.js
+++ server/server/imports/hooks/sendPushNotification.js
@@ -2,16 +2,20 @@ import rp from 'request-promise'
 
 export default (playerDevices, title, body) => {
   for (const device of playerDevices) {
+    const token = `ExponentPushToken[${device.pushToken}]`
+    console.log('token:', token, '\n')
     rp({
       uri: 'https://exp.host/--/api/v2/push/send',
       method: 'POST',
       json: true,
       body: {
-        to: device.pushToken,
+        to: token,
         title,
         body,
         priority: 'high',
         data: {
+          to: token,
+          badge: 1,
           title,
           body,
           priority: 'high',
