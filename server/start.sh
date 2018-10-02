3e1a98de663b1da0cdb3f46759485c9c133bc37c
Joaquín Gumucio <jjgumucio@gmail.com>
2018-10-02 19:52:31 -0300

    various

--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,5 @@
 export MAIL_URL='smtps://AKIAJGXSFCB6AJMHYUPQ:Ascd9ltYFJ03Vr3/LlN5FhbimBLLtUdd8qC7Q6Juundo@email-smtp.us-east-1.amazonaws.com:465'
 export MAIL_FROM='no-reply@acematch.cl'
+export MONGO_URL='mongodb://acematch-app:V46qRzTeXPHLd8ZJ@ds121743-a0.mlab.com:21743,ds121743-a1.mlab.com:21743/acematch-production?replicaSet=rs-ds121743'
 meteor --settings=settings.json
+
